3cb4713f7d12d7ecd778565c5a0a71164f6da66d
nestor-labiuk <nestorlabiuk@gmail.com>
2023-03-05 21:58:05 -0300

    agrega id al list item

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ function App() {
   const handleChange = (event) => {
     event.preventDefault()
     setNote(event.target.value)
+    console.log(event.target.value)
   }
 
   const handleSubmit = (event) => {
     event.preventDefault()
     note && setNewNote(newNote.concat(note))
+    const toAddToNewNote = {
+      content: note,
+      id: event.timeStamp
+    }
+    note && setNewNote(newNote.concat(toAddToNewNote))
+    console.log(toAddToNewNote)
     setNote('')
   }
 
   const reset = (event) => {
+    event.preventDefault()
     console.log(event.target)
-    console.log(event.target.lenght)
   }
 
-
+  
 
   return (
     <div className="app">
@@ -45,34 +52,31 @@ function App() {
 
             <div className="d-flex justify-content-center">
               <button className="btn btn-light" style={{ width: "6rem" }}
-              // type="submit" el ultimo boton de un form es submit or defecto
-              // onClick={handleClick}cambiamos el evento on click del boton por onSubmit del form
+                type="submit" // el ultimo boton de un form es submit or defecto
+              // onClick={handleClick}cambiamos el evento onClick del boton por onSubmit del form
               >Guardar </button>
             </div>
 
           </div>
 
-          {newNote &&
-            newNote.map((note) =>
-              <ul className="list-group">
-                <li className="input-group flex-nowrap my-1">
-
-                  <div className=" p-2 pt-2 rounded-1  border-3" style={{ backgroundColor: "#05d8" }}><input className="form-check-input" type="checkbox" id="flexCheckDefault" /></div>
-                  <input type="text" className="form-control " disabled value={note} />
-                  <span className="input-group-text " id="addon-wrapping">
-                    <div class="form-check ">
-                      <label className="form-check-label" for="flexCheckDefault">
-                      </label>
-
-                      <i className="bi bi-trash3 btn p-0"
-                        onClick={reset}
-                      ></i>
-                    </div>
+          <ul className="list-group">
+            {newNote &&
+              newNote.map((itemNote) =>
+                <li className="input-group flex-nowrap my-1" key={itemNote.id}>
+                  <div className=" p-2 pt-2 rounded-1  border-3" style={{ backgroundColor: "#05d8" }}>
+                    <input className="form-check-input" type="checkbox" id="flexCheckDefault" />
+                    <label className="form-check-label" for="flexCheckDefault"></label>
+                  </div>
+                  <input type="text" className="form-control " disabled value={itemNote.content} />
+                  <span className="input-group-text" id="addon-wrapping">
+                    <button type="reset" >
+                      <i className="bi bi-trash3 btn p-0"></i>
+                    </button>
                   </span>
                 </li>
-              </ul>
-            )
-          }
+              )
+            }
+          </ul>
 
         </form>
       </div>
